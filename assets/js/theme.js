4cb70474cbf4720ed063e2e7830d1af84e4187b2
SBouchard01 <simonbouchard47@gmail.com>
2025-07-31 23:53:02 +0200

    Update theme handling to use site-specific default value

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,5 @@
+---
+---
 // Updates the theme based on user preference or if toggled
 function updateTheme(theme) {
     document.documentElement.setAttribute('data-theme', theme);
@@ -7,13 +9,13 @@ function updateTheme(theme) {
 // toggle between light and dark themes when called
 async function toggleTheme() {
     const currentTheme = document.documentElement.getAttribute('data-theme');
-    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    const newTheme = currentTheme === 'light' ? 'dark' : '{{ site.css_theme }}';
     updateTheme(newTheme);
 }
 
 // Call updateTheme before page load
 (function() { 
-    const savedTheme = localStorage.getItem('theme') || 'light'; 
+    const savedTheme = localStorage.getItem('theme') || '{{ site.css_theme }}'; 
     // document.documentElement.setAttribute('data-theme', savedTheme); 
     updateTheme(savedTheme);
     console.log(`Theme set to ${savedTheme} on page load.`);
